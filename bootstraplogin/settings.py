8f96996cf22ee3798c22d36d9799db352b7011af
adarshp007 <adarshmp123@gmail.com>
2021-05-13 14:38:08 +0530

    debug false

--- bootstraplogin/settings.py
+++ bootstraplogin/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&06h1=^f$lp$e(!!)*t&s^#2b8%efz)-qq$6056ai-w=1ef6y6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
